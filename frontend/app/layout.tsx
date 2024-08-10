a207d8369fd60f56f89ebeca86e02e3b3953e0be
R1shabh-Gupta <imailrishabhgupta@gmail.com>
2024-08-10 23:47:49 +0530

    init: Setup nextjs, tailwindcss, shadcn

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Rubik - Frontend Security Toolkit",
+  description:
+    "Rubik: The ultimate toolkit for frontend security, offering Automated Penetration Testing, TakedownGPT, 'Have I Been Pwned?' checks, copycat takedown, URL details, and more.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
