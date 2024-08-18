fc7ecf04a45c7630ba68f74b85d611c1d9b5ccf3
R1shabh-Gupta <imailrishabhgupta@gmail.com>
2024-08-18 12:30:23 +0530

    feat(autotakedown): update autotakedown page

--- frontend/app/dashboard/autotakedown/page.tsx
+++ frontend/app/dashboard/autotakedown/page.tsx
@@ -3,6 +3,12 @@ import TakedownForm from "@/components/TakedownForm";
 const AutoTakedown = () => {
   return (
     <>
+      <div className="px-2 py-6 sm:py-0">
+        <h1 className="flex gap-1 text-2xl font-semibold">
+          <span className="text-[#E51015]">Takedown </span>Form
+        </h1>
+        <p className="text-sm font-medium">Enter takedown details</p>
+      </div>
       <TakedownForm />
     </>
   );
