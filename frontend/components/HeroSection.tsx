486760672a0b8e8655e987ec78409a8d23a8da31
R1shabh-Gupta <imailrishabhgupta@gmail.com>
2024-08-18 12:35:32 +0530

    feat: update HeroSection component

--- frontend/components/HeroSection.tsx
+++ frontend/components/HeroSection.tsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
     <div className="relative isolate overflow-hidden">
       <svg
-        className="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+        className="absolute inset-0 -z-10 h-full w-full dark:stroke-white/10 stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
         aria-hidden="true"
       >
         <defs>
@@ -19,7 +19,11 @@ export default function HeroSection() {
             <path d="M.5 200V.5H200" fill="none" />
           </pattern>
         </defs>
-        <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
+        <svg
+          x="50%"
+          y={-1}
+          className="overflow-visible hidden dark:flex fill-gray-800/20"
+        >
           <path
             d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
             strokeWidth={0}
@@ -44,22 +48,22 @@ export default function HeroSection() {
           }}
         />
       </div>
-      <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-20">
-        <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+      <div className="mx-auto max-w-7xl px-6 pb-24 sm:pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-16">
+        <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl">
           <div className="mt-24 sm:mt-32 lg:mt-16">
             <a href="#" className="inline-flex space-x-6">
               <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20">
                 What&apos;s new
               </span>
-              <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
+              <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 dark:text-gray-300 text-gray-600">
                 <span>Just shipped v1.0</span>
               </span>
             </a>
           </div>
-          <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          <h1 className="mt-10 text-4xl font-bold tracking-tight dark:text-white text-gray-900 sm:text-6xl">
             Secure Your Digital Presence with Rubik
           </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
+          <p className="mt-6 text-lg leading-8 dark:text-gray-300 text-gray-600">
             Rubik is your ultimate toolkit for automated penetration testing,
             breach detection, and domain security. Effortlessly safeguard your
             digital assets with our powerful, user-friendly, and 100% free
@@ -72,7 +76,10 @@ export default function HeroSection() {
                 Dashboard
               </span>
             </button>
-            <a href="#" className="text-sm font-semibold leading-6 text-white">
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 dark:text-white text-gray-900"
+            >
               Learn more <span aria-hidden="true">→</span>
             </a>
           </div>
@@ -80,11 +87,18 @@ export default function HeroSection() {
         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
             <Image
-              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+              src={`/images/dashboard-light.png`}
               alt="App screenshot"
               width={2432}
               height={1442}
-              className="w-[66rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
+              className="w-[62rem] dark:hidden flex rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
+            />
+            <Image
+              src={`/images/dashboard-dark.png`}
+              alt="App screenshot"
+              width={2432}
+              height={1442}
+              className="w-[62rem] dark:flex hidden rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
             />
           </div>
         </div>
