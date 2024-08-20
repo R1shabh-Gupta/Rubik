e70b9cd876eedef247d0fda2815b62bbc9fd7d67
R1shabh-Gupta <imailrishabhgupta@gmail.com>
2024-08-20 23:04:07 +0530

    feat(hero-section): update dashboard image and add Learn More button link

--- frontend/components/HeroSection.tsx
+++ frontend/components/HeroSection.tsx
@@ -77,7 +77,7 @@ export default function HeroSection() {
               </span>
             </button>
             <a
-              href="#"
+              href="#features"
               className="text-sm font-semibold leading-6 dark:text-white text-gray-900"
             >
               Learn more <span aria-hidden="true">→</span>
@@ -87,14 +87,14 @@ export default function HeroSection() {
         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
             <Image
-              src={`/images/dashboard-light.png`}
+              src={`/images/DomainTracer-light.png`}
               alt="App screenshot"
               width={2432}
               height={1442}
               className="w-[62rem] dark:hidden flex rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
             />
             <Image
-              src={`/images/dashboard-dark.png`}
+              src={`/images/DomainTracer-dark.png`}
               alt="App screenshot"
               width={2432}
               height={1442}
